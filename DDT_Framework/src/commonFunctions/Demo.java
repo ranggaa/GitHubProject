abb8e977e5e7f7ad70180aaa3247d2ee99bd3db0
MAX HUB <MAX HUB@DESKTOP-4M14A0P>
2023-07-24 19:34:58 +0530

    New Class Created with Methods

---
+++ DDT_Framework/src/commonFunctions/Demo.java
@@ -0,0 +1,16 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=345,b=98,c;
+ c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=9876,b=65,c;
+	 c=a+b;
+	System.out.println(c);
+}
+}
